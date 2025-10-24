1c5a6d2b25661e1f3d6514ac612523959e550fa3
ashenafi <ashenafiadola05@gmail.com>
2025-10-24 23:00:27 +0300

    setted up template files and view functions

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def register(request):
+
+    context = {
+
+    }
+    return render(request, 'blog/home.html',context)
+
+def signin(request):
+
+    context = {
+
+    }
+    return render(request, 'blog/home.html',context)
+
+def signout(request):
+
+    context = {
+
+    }
+    return render(request, 'blog/home.html',context)
+
+def home(request):
+
+    context = {
+
+    }
+    return render(request, 'blog/home.html',context)
