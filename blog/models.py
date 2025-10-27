8d597899f24cad82090971c3f95bfcc2153677f1
ashenafi <ashenafiadola05@gmail.com>
2025-10-27 13:17:42 +0300

    setted up static files, style sign in sign up template update register view function

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     author = models.CharField(max_length=255)
-    photo = models.ImageField(upload_to='static/blog')
+    photo = models.ImageField(upload_to='photos/')
     publication_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render, redirect
 from .forms import PostForm
 from .models import Post, Comment
+from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
-
+    page = 'register'
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('signin')
     context = {
-
+        'form':form,
+        'page':page,
     }
     return render(request, 'blog/signin_signup.html',context)
 
 def signin(request):
-
+    page = 'signin'
     context = {
-
+        'page':page,
     }
     return render(request, 'blog/signin_signup.html',context)
 
